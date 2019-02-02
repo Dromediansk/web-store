0f87249cbf35bcbcf3040067b0463a9bb60bd8fd
Dromediansk <pillar.mr@gmail.com>
2019-02-02 15:56:00 +0100

    handleDetail functionality

--- src/context.js
+++ src/context.js
@@ -25,9 +25,17 @@ class ProductProvider extends Component {
         return { products: tempProducts }
       })
     }
+    
+    getItem = (id) => {
+      const product = this.state.products.find(item => item.id === id);
+      return product;
+    }
 
-    handleDetail = () => {
-        console.log('hello from detail');
+    handleDetail = (id) => {
+      const product = this.getItem(id);
+      this.setState(() => {
+        return {detailProduct: product}
+      })
     }
 
     addToCart = (id) => {
