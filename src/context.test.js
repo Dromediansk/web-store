be7f6ae070e37b6e143329a5f41d7b1abe8f4e64
Dromediansk <pillar.mr@gmail.com>
2019-03-25 10:57:07 +0100

    removing unneccessary files

--- src/context.test.js
+++
@@ -1,7 +0,0 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import ProductProvider from './ProductProvider';
-
-it('expect to render ProductProvider component', () => {
-    expect(shallow(<ProductProvider />)).toMatchSnapshot();
-})
