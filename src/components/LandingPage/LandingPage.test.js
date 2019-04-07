039e9fb9810cab9e63fb0ff8fa28be99d99d5bbc
Dromediansk <pillar.mr@gmail.com>
2019-04-07 13:55:44 +0200

    deleting snapshot, minor style changes

--- src/components/LandingPage/LandingPage.test.js
+++
@@ -1,7 +0,0 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import LandingPage from './LandingPage';
-
-it('expect to render LandingPage component', () => {
-    expect(shallow(<LandingPage />)).toMatchSnapshot();
-})
