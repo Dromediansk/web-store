87c6cdb432e9d7c0b11a540a386e17e28af2993e
Pillár <Miroslav.Pillar@senacor.com>
2019-08-05 10:12:02 +0200

    svg in offer

--- src/components/LandingPage/Offer/Offer.js
+++ src/components/LandingPage/Offer/Offer.js
@@ -1,35 +1,32 @@
 import React from "react";
 import "./Offer.css";
 
+const profSvg = process.env.PUBLIC_URL + "/img/svg/professional.svg";
+const repairSvg = process.env.PUBLIC_URL + "/img/svg/repair.svg";
+const buySvg = process.env.PUBLIC_URL + "/img/svg/buy.svg";
+
 export default function Offer() {
   return (
     <div className="offer-section">
       <div className="offer-container">
         <div className="option">
           <div className="option-text">
-            <i className="fas fa-user-tie" />
+            <img src={profSvg} alt="professional" />
             <p>Proffessional approach</p>
           </div>
         </div>
         <div className="option">
           <a href="/store">
             <div className="option-text">
-              <i className="fas fa-shopping-cart" />
-              <p>
-                Choose
-                <br />& <br />
-                buy
-              </p>
+              <img src={buySvg} alt="buy" />
+              <p>Choose & buy</p>
             </div>
           </a>
         </div>
         <div className="option">
           <div className="option-text">
-            <i className="fas fa-tools" />
-            <p>
-              Repair <br />& <br />
-              configure
-            </p>
+            <img src={repairSvg} alt="repair" />
+            <p>Repair & configure</p>
           </div>
         </div>
       </div>
